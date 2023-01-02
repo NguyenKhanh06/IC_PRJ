ec23dea69f2b676c09f32ffae130f837db4c51dd
NguyenKhanh06 <nguyencongkhanh2906@gmail.com>
2023-01-02 21:01:03 +0700

    push code

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/header/Header';
+
+home.propTypes = {
+    
+};
+
+function home(props) {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
+
+export default home;
