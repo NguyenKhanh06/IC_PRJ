e5aa866e63259fa2687630f6573863aa9abfa038
NguyenKhanh06 <nguyencongkhanh2906@gmail.com>
2023-01-04 18:42:28 +0700

    update chat 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/students/Home';
-import Messenger from './pages/students/Messenger';
+import Messenger from './pages/chat/Messenger';
 
 function App() {
   return (
